854a6192e53fa64af1a6da22fc9b830e102824f1
SapheryDesings <sapherydesings@gmail.com>
2024-08-20 19:33:27 +0200

    astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SapheryDesings.github.io',
-  base: 'portfolioAstro',
+  base: '/portfolioAstro',
   integrations: [tailwind()]
 });
