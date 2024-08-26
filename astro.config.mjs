0ff574108493cf898562a248e4028d912f64c950
SapheryDesings <sapherydesings@gmail.com>
2024-08-26 19:30:13 +0200

    background y header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://SapheryDesings.github.io',
   base: '/portfolioAstro',
-  integrations: [tailwind()]
+  integrations: [tailwind(), icon()]
 });
